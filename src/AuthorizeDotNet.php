868192436038e09073ea800a40ebfd220996d898
Pablo de la Peña <p@shedcollective.org>
2018-01-30 15:28:53 +0000

    Support for charging a credit card directly

--- src/AuthorizeDotNet.php
+++ src/AuthorizeDotNet.php
@@ -22,37 +22,11 @@ use net\authorize\api\controller as AuthNetController;
 
 class AuthorizeDotNet extends PaymentBase
 {
+    /**
+     * The value of a successful response from the Authorize.Net API
+     * @type string
+     */
     const AUTH_NET_RESPONSE_OK = 'Ok';
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * The mode the API should be run in
-     * @var string
-     */
-    protected $sApiMode;
-
-    /**
-     * The Authorize.Net Authentication object
-     * @var AuthNetAPI\MerchantAuthenticationType
-     */
-    protected $oAuthentication;
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * AuthorizeDotNet constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->sApiMode = Environment::is('PRODUCTION') ? AuthNetConstants::PRODUCTION : AuthNetConstants::SANDBOX;
-
-        $this->oAuthentication = new AuthNetAPI\MerchantAuthenticationType();
-        $this->oAuthentication->setName($this->getSetting('sLoginId'));
-        $this->oAuthentication->setTransactionKey($this->getSetting('sTransactionKey'));
-    }
 
     // --------------------------------------------------------------------------
 
@@ -118,73 +92,62 @@ class AuthorizeDotNet extends PaymentBase
         $sFailUrl,
         $sContinueUrl
     ) {
-
         $oChargeResponse = Factory::factory('ChargeResponse', 'nailsapp/module-invoice');
 
         try {
+
+            //  Begin creating the charge request
+            $oCharge = new AuthNetAPI\TransactionRequestType();
+            $oCharge->setTransactionType('authCaptureTransaction');
 
             /**
              * If a payment_profile_id or customer_profile_id has been supplied then use these over
              * any supplied card details.
              */
 
-            $sPaymentProfileId  = property_exists($oCustomData, 'payment_profile_id') ? $oCustomData->payment_profile_id : null;
-            $sCustomerProfileId = property_exists($oCustomData, 'customer_profile_id') ? $oCustomData->customer_profile_id : null;
+            $sPaymentProfileId  = getFromArray('payment_profile_id', (array) $oCustomData);
+            $sCustomerProfileId = getFromArray('customer_profile_id', (array) $oCustomData);
+            $sCardNumber        = getFromArray('number', (array) $oData);
+            $oCardExpire        = getFromArray('exp', (array) $oData);
+            $sCardExpireMonth   = getFromArray('month', (array) $oCardExpire);
+            $sCardExpireYear    = getFromArray('year', (array) $oCardExpire);
+            $sCardCvc           = getFromArray('cvc', (array) $oData);
 
             if ($sPaymentProfileId || $sCustomerProfileId) {
-                if (empty($oCustomData->payment_profile_id)) {
-                    throw new DriverException('A Payment Profile ID must be supplied.');
-                } elseif (empty($oCustomData->customer_profile_id)) {
-                    throw new DriverException('A Customer Profile ID must be supplied.');
-                }
-
-                $oPaymentProfile = new AuthNetAPI\PaymentProfileType();
-                $oPaymentProfile->setPaymentProfileId($oCustomData->payment_profile_id);
-
-                $oCustomerProfile = new AuthNetAPI\CustomerProfilePaymentType();
-                $oCustomerProfile->setCustomerProfileId($oCustomData->customer_profile_id);
-                $oCustomerProfile->setPaymentProfile($oPaymentProfile);
-
+                $this->payUsingProfile($oCharge, $sPaymentProfileId, $sCustomerProfileId);
             } else {
-
+                $this->payUsingCardDetails($oCharge, $sCardNumber, $sCardExpireMonth, $sCardExpireYear, $sCardCvc);
             }
 
-            dumpanddie($oData, $oCustomData);
-
-            $oCharge = new AuthNetAPI\TransactionRequestType();
-            $oCharge->setTransactionType('authCaptureTransaction');
             $oCharge->setCurrencyCode($sCurrency);
             $oCharge->setAmount($iAmount / 100);
-            $oCharge->setProfile($oCustomerProfile);
 
             $oApiRequest = new AuthNetAPI\CreateTransactionRequest();
-            $oApiRequest->setMerchantAuthentication($this->oAuthentication);
+            $oApiRequest->setMerchantAuthentication($this->getAuthentication());
             $oApiRequest->setTransactionRequest($oCharge);
 
             $oApiController = new AuthNetController\CreateTransactionController($oApiRequest);
-            $oResponse      = $oApiController->executeWithApiResponse($this->sApiMode);
+            $oResponse      = $oApiController->executeWithApiResponse($this->getApiMode());
 
             if ($oResponse->getMessages()->getResultCode() === static::AUTH_NET_RESPONSE_OK) {
 
                 $oChargeResponse->setStatusComplete();
                 $oChargeResponse->setTxnId($oResponse->getTransactionResponse()->getTransId());
+                //  @todo (Pablo - 2018-01-30) - calculate and set the fee charged
 
             } else {
 
+                $oGeneralError  = reset($oResponse->getMessages()->getMessage());
+                $oSpecificError = reset($oResponse->getTransactionResponse()->getErrors());
+
                 $oChargeResponse->setStatusFailed(
-                    implode("\n\t-", array_map(
-                        function ($oMessage) {
-                            return $oMessage->getCode() . ': ' . $oMessage->getText() . "\n";
-                        },
-                        $oResponse->getMessages()->getMessage()
-                    )),
-                    0,
+                    $oGeneralError->getText() . '(' . $oSpecificError->getErrorCode() . ': ' . $oSpecificError->getErrorText() . ')',
+                    $oGeneralError->getCode(),
                     'The gateway rejected the request, you may wish to try again.'
                 );
             }
 
         } catch (\Exception $e) {
-
             $oChargeResponse->setStatusFailed(
                 $e->getMessage(),
                 $e->getCode(),
@@ -231,6 +194,7 @@ class AuthorizeDotNet extends PaymentBase
      */
     public function refund($sTxnId, $iAmount, $sCurrency, $oCustomData, $sReason, $oPayment, $oInvoice)
     {
+        dumpanddie('@todo');
         $oRefundResponse = Factory::factory('RefundResponse', 'nailsapp/module-invoice');
 
         try {
@@ -258,11 +222,11 @@ class AuthorizeDotNet extends PaymentBase
             $oCharge->setProfile($oCustomerProfile);
 
             $oApiRequest = new AuthNetAPI\CreateTransactionRequest();
-            $oApiRequest->setMerchantAuthentication($this->oAuthentication);
+            $oApiRequest->setMerchantAuthentication($this->getAuthentication());
             $oApiRequest->setTransactionRequest($oCharge);
 
             $oApiController = new AuthNetController\CreateTransactionController($oApiRequest);
-            $oResponse      = $oApiController->executeWithApiResponse($this->sApiMode);
+            $oResponse      = $oApiController->executeWithApiResponse($this->getApiMode());
 
             if ($oResponse->getMessages()->getResultCode() === static::AUTH_NET_RESPONSE_OK) {
 
@@ -297,110 +261,87 @@ class AuthorizeDotNet extends PaymentBase
      * Returns the mode to be used when running an Authorize.Net SDK controller.
      * @return string
      */
-    public function getApiMode()
+    protected function getApiMode()
     {
-        return $this->sApiMode;
+        return Environment::is('PRODUCTION') ? AuthNetConstants::PRODUCTION : AuthNetConstants::SANDBOX;
     }
 
     // --------------------------------------------------------------------------
 
     /**
-     * Returns a pre-configured authentication object to be used with Authorize.Net SDK requests.
-     * @return \net\authorize\api\contract\v1\MerchantAuthenticationType
+     * Returns an Authorize.Net authentication object
+     * @return AuthNetAPI\MerchantAuthenticationType
      */
-    public function getAuthentication()
+    protected function getAuthentication()
     {
-        return clone $this->oAuthentication;
+        $oAuthentication = new AuthNetAPI\MerchantAuthenticationType();
+        $oAuthentication->setName($this->getSetting('sLoginId'));
+        $oAuthentication->setTransactionKey($this->getSetting('sTransactionKey'));
+        return $oAuthentication;
     }
 
     // --------------------------------------------------------------------------
 
     /**
-     * Adds a payment source to a customer
+     * Complete the charge using an existing payment profile.
      *
-     * @param  $iCustomerId  integer      The customer ID to associate the payment source with
-     * @param  $mSourceData  string|array The payment source data to pass to Stripe; a token or an associative array
-     * @param  $sSourceLabel string       The label (or nickname) to give the card
+     * @param AuthNetAPI\TransactionRequestType $oCharge     The Charge object
+     * @param integer                           $iProfileId  The Payment profile ID
+     * @param integer                           $iCustomerId The customer profile ID
      *
-     * @return \stdClass                  The source object
      * @throws DriverException
      */
-    public function addPaymentSource($iCustomerId, $mSourceData, $sSourceLabel = '')
+    protected function payUsingProfile($oCharge, $iProfileId, $iCustomerId)
     {
-        //  @todo (Pablo - 2018-01-29) - Create source with Authorize.Net
-
-        $oAuthNetSourceModel = Factory::model('Source', 'nailsapp/driver-invoice-authorize-net');
-        $oSource             = $oAuthNetSourceModel->create(
-            [
-                'label'       => $sSourceLabel ?: $oSource->brand . ' card ending in ' . $oSource->last4,
-                'customer_id' => $iCustomerId,
-                'stripe_id'   => $oSource->id,
-                'last4'       => $oSource->last4,
-                'brand'       => $oSource->brand,
-                'exp_month'   => $oSource->exp_month,
-                'exp_year'    => $oSource->exp_year,
-                'name'        => $oSource->name,
-            ],
-            true
-        );
-
-        if (!$oSource) {
-            throw new DriverException(
-                'Failed to save payment source. ' . $oAuthNetSourceModel->lastError()
-            );
+        if (empty($iProfileId)) {
+            throw new DriverException('A Payment Profile ID must be supplied.');
+        } elseif (empty($iCustomerId)) {
+            throw new DriverException('A Customer Profile ID must be supplied.');
         }
 
-        return $oSource;
+        $oPaymentProfile = new AuthNetAPI\PaymentProfileType();
+        $oPaymentProfile->setPaymentProfileId($iProfileId);
+
+        $oCustomerProfile = new AuthNetAPI\CustomerProfilePaymentType();
+        $oCustomerProfile->setCustomerProfileId($iCustomerId);
+        $oCustomerProfile->setPaymentProfile($oPaymentProfile);
+
+        $oCharge->setProfile($oCustomerProfile);
     }
 
     // --------------------------------------------------------------------------
 
     /**
-     * Deletes a customer payment source
+     * Complete the charge using credit card details
      *
-     * @param integer $iCustomerId The customer ID to associate the payment source with
-     * @param integer $iSourceId   The payment source ID
+     * @param AuthNetAPI\TransactionRequestType $oCharge          the Charge object
+     * @param string                            $sCardNumber      The Card's number
+     * @param integer                           $iCardExpireMonth The Card's expiry month
+     * @param integer                           $iCardExpireYear  The Card's expiry year
+     * @param string                            $sCardCvc         The Card's CVC
      *
-     * @return bool
      * @throws DriverException
      */
-    public function removePaymentSource($iCustomerId, $iSourceId)
+    protected function payUsingCardDetails($oCharge, $sCardNumber, $iCardExpireMonth, $iCardExpireYear, $sCardCvc)
     {
-        $oAuthNetSourceModel = Factory::model('Source', 'nailsapp/driver-invoice-authorize-net');
-        $aSources            = $oAuthNetSourceModel->getAll([
-            'where' => [
-                ['id', $iSourceId],
-                ['customer_id', $iCustomerId],
-            ],
-        ]);
-
-        if (count($aSources) !== 1) {
-            throw new DriverException('Not a valid payment source for customer #' . $iCustomerId);
+        if (empty($sCardNumber)) {
+            throw new DriverException('Card number must be supplied.');
+        } elseif (empty($iCardExpireMonth)) {
+            throw new DriverException('Card expiry month must be supplied.');
+        } elseif (empty($iCardExpireYear)) {
+            throw new DriverException('Card expiry year must be supplied.');
+        } elseif (empty($sCardCvc)) {
+            throw new DriverException('Card CVC number must be supplied.');
         }
 
-        $oSource = reset($aSources);
+        $oCreditCard = new AuthNetAPI\CreditCardType();
+        $oCreditCard->setCardNumber($sCardNumber);
+        $oCreditCard->setExpirationDate($iCardExpireYear . '-' . $iCardExpireMonth);
+        $oCreditCard->setCardCode($sCardCvc);
 
-        //  @todo (Pablo - 2018-01-29) - Delete from Authorze.Net
+        $oPayment = new AuthNetAPI\PaymentType();
+        $oPayment->setCreditCard($oCreditCard);
 
-        return $oAuthNetSourceModel->delete($oSource->id);
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Returns an array of Authorize.Net payment sources for a particular customer ID
-     *
-     * @param $iCustomerId integer The customer ID to retrieve for
-     *
-     * @return array
-     */
-    public function getPaymentSources($iCustomerId)
-    {
-        $oAuthNetSourceModel = Factory::model('Source', 'nailsapp/driver-invoice-authorize-net');
-        return $oAuthNetSourceModel->getAll([
-            'where' => [
-                ['customer_id', $iCustomerId],
-            ],
-        ]);
+        $oCharge->setPayment($oPayment);
     }
 }
