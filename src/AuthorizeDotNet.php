6b59c436fb21daea20606b4d3f0e38799fbb4c46
Pablo de la Peña <p@shedcollective.org>
2019-09-05 12:09:46 +0100

    Adds placeholder `createSource` method

--- src/AuthorizeDotNet.php
+++ src/AuthorizeDotNet.php
@@ -12,6 +12,7 @@
 
 namespace Nails\Invoice\Driver\Payment;
 
+use Nails\Common\Exception\NailsException;
 use Nails\Environment;
 use Nails\Factory;
 use Nails\Invoice\Driver\Payment\AuthorizeDotNet\Constants;
@@ -578,5 +579,24 @@ class AuthorizeDotNet extends PaymentBase
             $oGeneralError  = reset($aGeneralErrors);
             throw new DriverException($oGeneralError->getCode() . ': ' . $oGeneralError->getText());
         }
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Creates a new payment source, returns a semi-populated source resource
+     *
+     * @param \Nails\Invoice\Resource\Source $oResource The Resouce object to update
+     * @param array                          $aData     Data passed from the caller
+     *
+     * @throws DriverException
+     */
+    public function createSource(
+        \Nails\Invoice\Resource\Source &$oResource,
+        array $aData
+    ): void
+    {
+        //  @todo (Pablo - 2019-09-05) - implement this
+        throw new NailsException('Method not implemented');
     }
 }
